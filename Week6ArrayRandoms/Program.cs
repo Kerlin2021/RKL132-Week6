304e2e9e1048a473f3635f87c469644fbaab805b
Kerlin Pennula <Pennulakerlin@gmail.com>
2022-12-12 18:12:37 +0200

    add Week6 code

---
+++ Week6ArrayRandoms/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "1,2,3", "4,5,6", "7,8,9" };
+
+Random rnd = new Random();
+
+int randomindex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomindex]}");
---
+++ Week6ArrayWhitNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = new int[10];
+Random rnd = new Random();
+
+int sum = 0;
+
+// populating the array
+
+string hello = "hello, Word!".string();
+int counter = 0;
+for (int i = 0; i< hello.lenght; i++)
+{
+    if (hello[i] == 'l')
+    {
+        counter++;
+    }
+}
